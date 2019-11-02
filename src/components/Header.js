253b27605d667a315b4304be01607609c43e52b4
Alex Wong <alex.wong@clientrelationship.com>
2019-11-02 20:27:37 +0000

    Changed Secret Santa Bar text colour.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,8 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Bar = styled.div`
-  background-color: blue;
+  background-color: green;
+  color: white;
   width: 100%;
   height: 50px;
 `

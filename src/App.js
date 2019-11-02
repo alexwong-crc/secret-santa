9c0098fb948dc22ed0ca00355202a4339ad9f502
Alex Wong <alex.wong@clientrelationship.com>
2019-11-02 19:19:53 +0000

    Made Secret Santa stateless.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
-export default class App extends React.Component {
-  render() {
-    return <div>Secret Santa</div>
-  }
+
+const App = () => {
+  return <div>Secret Santa</div>
 }
+
+export default App

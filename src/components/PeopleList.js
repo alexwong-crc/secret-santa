22d4c8e37a85d6e20ccba53462d5e88303ebfeea
Alex Wong <alex.wong@clientrelationship.com>
2019-11-02 23:51:03 +0000

    Made people list into a class component.

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import PersonInput from './PersonInput'
 
-export const PeopleList = () => {
-  return (
-    <React.Fragment>
-      <PersonInput />
-      <button>+</button>
-    </React.Fragment>
-  )
+export class PeopleList extends React.Component {
+  state = {
+    peopleCount: 1,
+    foo: 'bar'
+  }
+
+  increasePeopleCount = () => {
+    this.setState(prevState => {
+      return {
+        peopleCount: prevState.peopleCount + 1
+      }
+    })
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        {this.state.peopleCount}
+        <PersonInput />
+        <button onClick={this.increasePeopleCount}>+</button>
+      </React.Fragment>
+    )
+  }
 }

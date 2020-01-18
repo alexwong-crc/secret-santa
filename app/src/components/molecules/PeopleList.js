9aab8eb9f1dd47daed513c611d47da65f27d71a5
Alex Wong <alex.wong@clientrelationship.com>
2020-01-18 23:34:15 +0000

    refactored to use hooks

--- app/src/components/molecules/PeopleList.js
+++ app/src/components/molecules/PeopleList.js
@@ -1,34 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import PersonInput from "./PersonInput";
 
-export class PeopleList extends React.Component {
-  state = {
-    peopleCount: 1
-  };
+export const PeopleList = () => {
+  const [peopleCount, setPeopleCount] = useState(1);
 
-  increasePeopleCount = () => {
-    this.setState(prevState => {
-      return {
-        peopleCount: prevState.peopleCount + 1
-      };
-    });
-  };
-  renderPersonInput = () => {
+  const renderPersonInput = () => {
     const personInputArray = [];
-    // return <PersonInput />
-    for (let i = 0; i < this.state.peopleCount; i++) {
-      //the key below needs to be changed because using numbers as keysis bad practise.
+    for (let i = 0; i < peopleCount; i++) {
       personInputArray.push(<PersonInput key={i} />);
     }
     return personInputArray;
   };
 
-  render() {
-    return (
-      <React.Fragment>
-        {this.renderPersonInput()}
-        <button onClick={this.increasePeopleCount}>+</button>
-      </React.Fragment>
-    );
-  }
-}
+  const increasePeopleCount = () => {
+    setPeopleCount(peopleCount + 1);
+  };
+
+  return (
+    <>
+      {renderPersonInput()}
+      <button onClick={increasePeopleCount}>+</button>
+    </>
+  );
+};

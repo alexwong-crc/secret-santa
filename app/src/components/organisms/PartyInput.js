87531f3ae7eb47c99cb5bce29e0f3f0bfae1853c
Alex Wong <alex.wong@clientrelationship.com>
2020-01-19 09:35:16 +0000

    created basic form with formik

--- app/src/components/organisms/PartyInput.js
+++ app/src/components/organisms/PartyInput.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { PeopleList } from "../molecules/PeopleList";
+import { Formik } from "formik";
+
+const formInitialValues = {
+  partyName: ""
+};
 
 export default () => {
+  const formOnSubmit = props => {
+    console.log(props);
+  };
   return (
     <div>
-      <div>
-        <label for="partyName">Party Name:</label>
-        <input name="partyName" id="partyName" />
-      </div>
+      <Formik initialValues={formInitialValues} onSubmit={formOnSubmit}>
+        {props => (
+          <form onSubmit={props.handleSubmit}>
+            <label htmlFor="partyName">Party Name:</label>
+            <input
+              name="partyName"
+              id="partyName"
+              type="text"
+              onBlur={props.handleBlur}
+              value={props.values.partyName}
+              onChange={props.handleChange}
+            />
+            <button type="submit">Submit</button>
+          </form>
+        )}
+      </Formik>
       <PeopleList />
     </div>
   );

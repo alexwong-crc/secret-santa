39cc45dc4c8dd895d5a6354e58b365e3e3e27c9c
Alex Wong <alex.wong@clientrelationship.com>
2020-04-26 11:15:19 +0100

    added basic form component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Header, Divider } from '@/atoms';
+import { Form } from '@/organisms';
 import GlobalStyle from '@/styles/GlobalStyle';
 
 const App: React.FC = () => (
@@ -8,6 +9,7 @@ const App: React.FC = () => (
     <Container>
       <Header centre>Secret Santa</Header>
       <Divider />
+      <Form />
     </Container>
   </>
 );

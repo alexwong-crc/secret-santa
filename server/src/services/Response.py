dac59eaff98fe06aeb2e61ffe6a351988eee2c36
Alex Wong <alex.wong@clientrelationship.com>
2020-05-25 14:59:35 +0100

    fixed serverless config, where stage was not applied correctly. updated serverless to allow cors for only dev stage

--- server/src/services/Response.py
+++ server/src/services/Response.py
@@ -1,10 +1,23 @@
 import json
+import os
 
 
 class ResponseClass:
+    def __init__(self):
+        self.corsHeader = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": True,
+        }
+
     def make(self, status: int, body):
+        headers = {"Content-Type": "application/json", "charset": "UTF-8"}
+
+        # Apply CORS headers for dev due to localhost
+        if os.environ.get("stage") == "dev":
+            headers = {**headers, **self.corsHeader}
+
         return {
             "statusCode": status,
-            "headers": {"Content-Type": "application/json", "charset": "UTF-8"},
+            "headers": headers,
             "body": json.dumps({"result": body}),
         }

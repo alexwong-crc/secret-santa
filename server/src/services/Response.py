05709fba60f670d2a9c7ebed970a9115ead3800d
Alex Wong <alex.wong@clientrelationship.com>
2020-05-30 00:26:09 +0100

    added cors for prod

--- server/src/services/Response.py
+++ server/src/services/Response.py
@@ -4,13 +4,14 @@ import os
 
 class ResponseClass:
     def __init__(self):
-        self.corsHeader = {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Credentials": True,
-        }
+        self.corsHeader = {"dev": "*", "prod": "https://santa.alexhomingwong.co.uk/*"}
 
     def make(self, status: int, body):
-        headers = {"Content-Type": "application/json", "charset": "UTF-8"}
+        headers = {
+            "Content-Type": "application/json",
+            "charset": "UTF-8",
+            "Access-Control-Allow-Origin": self.corsHeader[os.environ.get("stage")],
+        }
 
         # Apply CORS headers for dev due to localhost
         if os.environ.get("stage") == "dev":
